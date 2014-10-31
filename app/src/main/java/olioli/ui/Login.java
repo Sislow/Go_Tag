bb3f7381e0780c387eda98707ea71c75835a53db
hardwiog <hardwiog@mail.uc.edu>
2014-10-31 16:36:20 -0400

    Update Login.java

--- app/src/main/java/olioli/ui/Login.java
+++ app/src/main/java/olioli/ui/Login.java
@@ -1,48 +1,50 @@
 package olioli.ui;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.location.Criteria;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import java.io.InputStream;
-
-import olioli.tag.GPSTracker;
+import olioli.dao.NetworkConnect;
 import olioli.tag.R;
 
 /**
  * Created by Ohardwick on 9/16/14.
- * FAKE LOGIN SCREEN
+ *
+ * Establishing auto login &
+ * Register
+ *
  */
 public class Login extends Activity {
+
+    NetworkConnect nc;
+    Button sign_In;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-        Button sign = (Button) findViewById(R.id.sign);
+        setContentView(R.layout.login_activity);
 
-        GPSTracker mGPS = new GPSTracker(this);
+        // Establish button
+        sign_In = (Button) findViewById(R.id.btn_auto_sign);
 
-        TextView text = (TextView) findViewById(R.id.texts);
-        if(mGPS.canGetLocation ){
-            mGPS.getLocation();
-            text.setText("Lat"+mGPS.getLatitude()+"Lon"+mGPS.getLongitude());
-        }else{
-            text.setText("Unabletofind");
-            System.out.println("Unable");
-        }
-        getSharedPreferences("", Context.MODE_PRIVATE);
-        sign.setOnClickListener(new View.OnClickListener() {
+        // Establish connnection before starting game
+        nc = new NetworkConnect();
+        nc.connect();
+
+
+
+        sign_In.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent next = new Intent(Login.this, CurrentGame.class);
-                startActivity(next);
-            }
+
+                    Intent next = new Intent(Login.this, CurrentGame.class);
+                    startActivity(next);
+
+                }
+
+
         });
     }
 }
